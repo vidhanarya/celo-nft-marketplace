44c6277a992105332c359e57ea83beba6f7ed95b
vidhanarya <vidhanarya50@gmail.com>
2022-11-03 14:27:30 +0530

    Create Celo NFT Marketplace

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/vidhanarya/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0x7bE1CA125f8d54366dbc8AAaaDc0C68Cdb78D98a";
